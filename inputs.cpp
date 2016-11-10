f7491ec412f378438da2b52eee170803a64b6791
atukenov <almaz.t97@gmail.com>
2016-11-09 19:57:32 -0600

    Output test cases

--- inputs.cpp
+++ inputs.cpp
@@ -11,18 +11,17 @@ char N[10];
 
 int main()
 {
-	srand(time(NULL));
+	srand(time(NULL)); 
 	for (int i = 0; i <= 20; ++i)
 	{
 		filename = name;
 		if (i < 10)
-			filename = filename + '0' + to_string(i);
+			filename = filename + '0' + to_string(i); // files input01 .. input09
 		else
 		{
-			filename = filename + to_string(i);
+			filename = filename + to_string(i); // files input10 and more
 		}
-		freopen(filename.c_str(), "w", stdout);
-		cout << rand() % 100 + 1 << ' ' << rand() % 100 + 1;
+		freopen(filename.c_str(), "w", stdout); // create filename
+		cout << rand() % 100 + 1 << ' ' << rand() % 100 + 1;  // add two numbers
 	}
-
 }
---
+++ outputs.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <cstdio>
+
+using namespace std;
+
+string filename, outputname;
+
+int n, m;
+
+int solve(int n, int m)
+{
+    int s = 0;
+    s = n + n / (m - 1);
+    if (n % (m - 1) == 0)
+        s -= 1;
+    return s;
+}
+
+int main()
+{
+	srand(time(NULL)); 
+	for (int i = 2; i <= 20; ++i)
+	{
+		filename = "input";
+		outputname = "output";
+		if (i < 10)
+		{
+			filename = filename + '0' + to_string(i),
+			outputname = outputname + '0' + to_string(i);  // files input01 .. input09
+		}
+		else
+		{
+			filename = filename + to_string(i),
+			outputname = outputname + to_string(i);	 // files input10 and more
+		}
+		filename = "TESTS/" + filename;
+		outputname = "TESTS/" + outputname;
+		freopen(filename.c_str(), "r", stdin); // create filename
+		freopen(outputname.c_str(), "w", stdout); // create output with answer
+		// --------YOUR SOLUTION ACCEPTED SOLUTION------------------
+		scanf("%d %d\n", &n, &m);
+    	cout << solve(n, m) << endl;
+    	//----------------------------------------------------------
+    	fclose(stdin);
+    	fclose(stdout);
+	}
+}
+
